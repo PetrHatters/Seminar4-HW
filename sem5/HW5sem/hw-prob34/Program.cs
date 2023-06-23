18ff407aed557bf387aa3f24d0e113be2f09adb2
PetrHatters <psy.entry@gmail.com>
2023-06-24 02:26:14 +0400

    hw5 done

---
+++ sem5/HW5sem/hw-prob34/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 34: Задайте массив заполненный 
+случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+int CountEvenNumbers(int[] array)
+{
+int count = 0;
+foreach (var item in array)
+{
+    if( item%2==0) count ++;
+}
+return count;
+}
+
+int[] arr = GetRandomArray(8, 100, 1000);
+Console.WriteLine(String.Join("; ", arr) + $" Четных чисел {CountEvenNumbers(arr)}");
+
---
+++ sem5/HW5sem/hw-prob38/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементами массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+*/
+double[] arr = new double[] { 3.22, 4.2, 1.15, 77.15, 65.2 };
+Console.WriteLine(Subtract(FindMin(arr), FindMax(arr)));
+
+double Subtract(double minValue, double maxValue)
+{
+
+    double subtaction = maxValue - minValue;
+
+    return subtaction;
+
+}
+double FindMax(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i]) max = array[i];
+    }
+    return max;
+}
+double FindMin(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
+}
